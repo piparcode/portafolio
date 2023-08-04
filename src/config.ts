a950a8c0e9605909685b6b59b63deb52ad94639b
deibypena <deiby.penaa@gmail.com>
2023-08-03 19:05:15 -0500

    no jodan xd

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Deiby peña';
+export const SITE_DESCRIPTION = 'Un apasionado del desarrollo de software, me gusta resolver problemas y crear soluciones innovadoras que mejoran la vida de las personas.';
